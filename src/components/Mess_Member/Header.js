668e76cd340c25952c0ac47d0a3499a3c119eaa6
iit2019017 <iit2019017@iiita.ac.in>
2020-12-19 15:41:59 +0530

    changed classname

--- src/components/Mess_Member/Header.js
+++ src/components/Mess_Member/Header.js
@@ -9,19 +9,19 @@ function Header() {
       <div className="row block">
       <h1 className="food">Breakfast</h1>
       <Link to="/MessMenu">
-      <button>Add Item</button>
+      <button className="additem">Add Item</button>
       </Link>
       </div>
       <div className="block row">
     <h1 className="food">Lunch</h1>
     <Link to="/MessMenu">
-      <button>Add Item</button>
+      <button className="additem">Add Item</button>
       </Link>
   </div>
   <div className="row block">  
       <h1 className="food">Dinner</h1>
       <Link to="/MessMenu">
-      <button>Add Item</button>
+      <button className="additem">Add Item</button>
       </Link>
       
     </div>
---
+++ src/components/Mess_Member/member.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Style.css';
+import Header from "./Header";
+import Footer from "./Footer";
+import Head from "./Head";
+
+function Member(){
+    return(
+        <div>
+            <Head />
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+
+export default Member;
